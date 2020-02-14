1ce8e616d8c1f652b551531c8bd1c51ff7e0e800
19950512 <36517599+19950512@users.noreply.github.com>
2020-02-14 13:10:31 -0300

    back & next
    -

--- js/js/galeria.js
+++ js/js/galeria.js
@@ -4,7 +4,7 @@ class Galeria{
 
 		// Elemento pai da galeria
 		this.galeria_container = this.id(configuracoes['galeria-id']);
-		
+
 		// Armazen todas as imagens da galeria
 		this.imagens = imagens;
 
@@ -20,13 +20,31 @@ class Galeria{
 		this.controllers = {
 			'left': this.galeria_container.querySelectorAll('.' + configuracoes.controllers['left']),
 			'right': this.galeria_container.querySelectorAll('.' + configuracoes.controllers['right']),
-			'play': null,
+			'play': this.galeria_container.querySelectorAll('.' + configuracoes.controllers['play']),
 			'fullscreen': null,
 			'close': null,
 		}
+	}
+
+	init(){
 
 		// Renderiza a galeria
 		this.render();
+
+		// Escuta as teclas - setinhas do teclado
+		document.addEventListener("keyup", function(evt){
+			
+			// seta direita 
+			if(evt.keyCode == 39){
+				galeria.next()
+			}
+
+			// seta esquerda 
+			if(evt.keyCode == 37){
+				galeria.back()
+			}
+
+		}, true);
 	}
 
 	/* Retorna um elemento pelo ID */
@@ -35,31 +53,119 @@ class Galeria{
 	}
 
 	/* Ação imagem anterior */
-	back(btn){
-		console.log(btn, 'estou voltando..');
+	back(){
+
+		if(this.imagens[this.imagem_current - 1]){
+
+			// Atualiza a imagem current
+			this.imagem_current = this.imagem_current - 1;
+		}
+
+		// Renderiza a galeria
+		this._renderMain();
+
+		this._renderRight()
+		this._renderLeft()
 	}
 
 	/* Ação proxima imagem */
-	next(btn){
-		console.log(btn, 'estou avançando..');
+	next(){
+
+		if(this.imagens[this.imagem_current + 1]){
+
+			// Atualiza a imagem current
+			this.imagem_current = this.imagem_current + 1;
+		}
+
+		// Renderiza a galeria
+		this._renderMain()
+
+		this._renderRight()
+		this._renderLeft()
 	}
 
+	/* Slide, status = true (ON) - status = false (OFF) */
+	slide(btn, status = true){
+
+		// Altera o botão play do slide
+		btn.textContent = !status ? 'ON' : 'OFF';
+		btn.setAttribute('onclick', 'galeria.slide(this, ' + !status + ')');
+	}
+
+	/* Renderiza imagem principal */
+	_renderMain(){
+
+		// Se a imagem atual existe na lista das imagens
+		if(this.imagens[this.imagem_current]){
+
+			// Renderiza a imagem current em todos os elementos MAIN (imagem principal)
+			for (var i = this.elements['main'].length - 1; i >= 0; i--) {
+				this.elements['main'][i].src = this.imagens[this.imagem_current]['src'];
+			}
+		}
+	}
 
 	/* Responsável por renderizar os botões */
 	_renderBotoes(){
+		this._renderRight()
+		this._renderLeft()
+		this._renderPlay()
+	}
+
+	/* Renderiza só a setinha Direita */
+	_renderRight(){
+
 		// Renderiza os botões de controle RIGHT
 		for (var i = this.controllers.right.length - 1; i >= 0; i--){
-			this.controllers.right[i].setAttribute('onclick', 'galeria.next(this)');
+
+			this.controllers.right[i].setAttribute('onclick', 'galeria.next()');
+
+			if(!this.imagens[this.imagem_current + 1]){
+				this.controllers.right[i].style.display = 'none';
+			}else{
+				this.controllers.right[i].style.display = null;		
+			}
 		}
+	}
+
+	/* Renderiza só a setinha Esquerda */
+	_renderLeft(){
 
 		// Renderiza os botões de controle LEFT
 		for (var i = this.controllers.left.length - 1; i >= 0; i--){
-			this.controllers.left[i].setAttribute('onclick', 'galeria.back(this)');
+
+			this.controllers.left[i].setAttribute('onclick', 'galeria.back()');
+
+			if(!this.imagens[this.imagem_current - 1]){
+				this.controllers.left[i].style.display = 'none';
+			}else{
+				this.controllers.left[i].style.display = null;		
+			}
 		}
 	}
+
+	/* Renderiza só o botão Play - Slide */
+	_renderPlay(){
+
+		// Renderiza os botões de controle PLAY
+		for (var i = this.controllers.play.length - 1; i >= 0; i--){
+
+			this.controllers.play[i].setAttribute('onclick', 'galeria.slide(this, true)');
+
+			if(!this.imagens[1]){
+				this.controllers.play[i].style.display = 'none';
+			}else{
+				this.controllers.play[i].style.display = null;		
+			}
+		}
+	}
+
 	render(){
 
 		// Renderiza os botões
 		this._renderBotoes()
+
+		// Renderiza imagem principal
+		this._renderMain();
 	}
 }
